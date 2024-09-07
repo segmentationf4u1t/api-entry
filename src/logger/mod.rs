ad1d9e7137935acbe0619456c6e35a3d71050a14
shef <cryptexunion@gmail.com>
2024-09-07 22:59:37 +0200

    Add logging enhancements to Logger middleware with info and error messages for request processing.

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -3,12 +3,14 @@ use actix_web::Error;
 use chrono::Utc;
 use futures::future::{ok, Ready};
 use futures::Future;
-use log::error;
+use log::{error, info};
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use log::info;
+
+// Logger struct for creating the middleware
 pub struct Logger;
 
+// Implementation of the Transform trait for Logger
 impl<S, B> Transform<S, ServiceRequest> for Logger
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
@@ -21,15 +23,18 @@ where
     type Transform = LoggerMiddleware<S>;
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
+    // Create a new LoggerMiddleware
     fn new_transform(&self, service: S) -> Self::Future {
         ok(LoggerMiddleware { service })
     }
 }
 
+// LoggerMiddleware struct that wraps the inner service
 pub struct LoggerMiddleware<S> {
     service: S,
 }
 
+// Implementation of the Service trait for LoggerMiddleware
 impl<S, B> Service<ServiceRequest> for LoggerMiddleware<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
@@ -40,10 +45,12 @@ where
     type Error = Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
 
+    // Check if the service is ready
     fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.service.poll_ready(cx)
     }
 
+    // Handle the incoming request
     fn call(&self, req: ServiceRequest) -> Self::Future {
         let start_time = Utc::now();
         let method = req.method().to_string();
@@ -55,11 +62,13 @@ where
             let res = fut.await?;
             let elapsed = Utc::now().signed_duration_since(start_time).num_milliseconds();
 
+            // Create log message
             let log_message = format!(
                 "Request processed - Timestamp: {}, Method: {}, Path: {}, Status: {}, Elapsed: {}ms",
                 start_time, method, path, res.status(), elapsed
             );
 
+            // Log based on response status
             if res.status().is_server_error() {
                 error!(target: "my_actix_api", "{}", log_message);
             } else {
